9ed1c655bf871d5182219776180c133c2967bf86
Altamash <heyboss238@gmail.com>
2025-05-29 00:25:28 +0530

    Terraform extra resource group added

--- module/resourcegroup/main.tf
+++ module/resourcegroup/main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "azure-stack-rs" {
   name     = var.name
   location = var.location
 }
+resource "azurerm_resource_group" "azure-rg" {
+  name     = "new896rg"
+  location = "westeurope"
+}
