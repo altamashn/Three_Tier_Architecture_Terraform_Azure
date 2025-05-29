d2c0af4fc76ebf099a9d6ed7d518e79e7ddd28ca
Altamash <heyboss238@gmail.com>
2025-05-29 20:52:13 +0530

    Second Change to the resource group file

--- module/resourcegroup/main.tf
+++ module/resourcegroup/main.tf
@@ -2,7 +2,3 @@ resource "azurerm_resource_group" "azure-stack-rs" {
   name     = var.name
   location = var.location
 }
-resource "azurerm_resource_group" "azure-rg" {
-  name     = "new896rg"
-  location = "westeurope"
-}
